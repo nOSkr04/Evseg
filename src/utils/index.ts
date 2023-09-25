1e8f59f5827f51f9c518602f9b71be5b8daf81e9
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2023-09-26 00:31:30 +0800

    sync

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,8 @@ import { authLogout } from "../store/auth-slice";
 
 export class HttpRequest extends BaseHttpRequest {
   // uri = "http://192.168.1.23:8001/api/v1api/v1";
-  uri = "http://192.168.1.62:3000";
+  // uri = "http://192.168.1.62:3000";
+  uri = "http://139.59.115.72";
   
   store = store;
   errorHandler = (statusCode: number, error: HttpHandler): void => {
